e98191998ba33ac3e29b77660771a20a75031b7a
anchu09 <d.anchuela@usp.ceu.es>
2023-02-24 21:16:45 +0100

    arreglado bug reglas

--- src/main/java/threadServer/ServerThreads.java
+++ src/main/java/threadServer/ServerThreads.java
@@ -7,11 +7,17 @@ import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import db.pojos.Questionary;
+
+
 public class ServerThreads {
 
     public static void main(String args[]) throws IOException {
-        //Create a service that is waiting in port 9000
-
+       
     	
     	ServerSocket serverSocket = new ServerSocket(9000);
         try {
